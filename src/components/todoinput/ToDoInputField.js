e6f9d6d22e773dd729157c363bb2c10e1884f112
RogueStar112 <hildtgunnt@gmail.com>
2022-11-03 14:49:36 +0000

    ui update 3

--- src/components/todoinput/ToDoInputField.js
+++ src/components/todoinput/ToDoInputField.js
@@ -8,10 +8,10 @@ import Col from 'react-bootstrap/Col';
 
 const ToDoInputField = (props) => {
     return (
-      <div className="mx-2" style={{position: "relative", width: "100%"}}>
-      <Form.Label size="sm" className="mx-2 d-none d-xl-block" style={{position: "absolute", bottom:"30px", display: "", fontSize: "10px"}}>{props.label}</Form.Label>
+      <div className="" style={{position: "relative", width: "100%"}}>
+      <Form.Label size="sm" className="mx-2" style={{position: "absolute", bottom:"30px", display: "", fontSize: "10px"}}>{props.label}</Form.Label>
 
-      <Form.Control className="mx-2" type={props.type} value={props.value} onChange={props.onChange} name={props.name} placeholder={props.placeholder} style={{width: "100%"}}>
+      <Form.Control className="" type={props.type} value={props.value} onChange={props.onChange} name={props.name} placeholder={props.placeholder} style={{width: "100%"}}>
       </Form.Control>
       </div>
     );
