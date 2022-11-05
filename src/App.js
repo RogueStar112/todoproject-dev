c4efe0f0de8fefce8b6c53272b405ca0ce1c18a9
RogueStar112 <hildtgunnt@gmail.com>
2022-11-05 00:14:54 +0000

    Mobile UI Update 8

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ const addTask = (userInput_task, userInput_tags, userInput_bg, userInput_date) =
 
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
   return (
-    <div className="app" style={{width: "98vw"}}>
+    <div className="app" style={{width: "95vw", margin: "0 auto"}}>
       <ToDoInputMobile addTask={addTask}/>
       <ToDoOutput toDoList={toDoList} counter={counter} />
       <ClearTasksButton />
